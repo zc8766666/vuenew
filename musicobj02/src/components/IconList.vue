2138c1e5cdebcba08f408fcfea414817100b0482
zhangcheng <194368104@qq.com>
2023-05-25 17:54:18 +0800

    5-25

---
+++ musicobj02/src/components/IconList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        我是iconlist
+    </div>
+</template>
+<script>
+export default {
+  name: "iconlist",
+};
+</script>
---
+++ musicobj02/src/components/MusicList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        我是musiclist
+    </div>
+</template>
+<script>
+export default {
+  name: "musiclist",
+};
+</script>
